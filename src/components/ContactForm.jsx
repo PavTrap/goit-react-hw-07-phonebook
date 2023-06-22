558e445f78de34230f572fa57091ae79afb8dcb7
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-06-22 16:20:07 +0300

    update pattern input

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -65,7 +65,7 @@ export default function NewContactForm(){
           name="name"
           value={name}
           onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -76,7 +76,7 @@ export default function NewContactForm(){
           className={css.input} 
           value={number}
           onChange={handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
